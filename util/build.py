6a9248dbc142e2bf134f1bb2a62fab88f6025b48
Marius <Chuddle@web.de>
2020-02-24 19:30:00 +0100

    Added some translations and comments

--- util/build.py
+++ util/build.py
@@ -4,26 +4,30 @@ import sys
 import argparse
 
 def main(input_data_path,output_data_path):
+	# Kommando für: Building mediapipe
 	comp='bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 \
     mediapipe/examples/desktop/multi_hand_tracking:multi_hand_tracking_cpu'
-    #명령어 컴파일
+    # Kommando für: MediaPipe ausführen um die Videodateien in Textfiles zu konvertieren
 	cmd='GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/multi_hand_tracking/multi_hand_tracking_cpu \
     --calculator_graph_config_file=mediapipe/graphs/hand_tracking/multi_hand_tracking_desktop_live.pbtxt'
-    #미디어 파이프 명령어 저장
+    #Liste aller Verzeichnisse im Input-Ordner
 	listfile=os.listdir(input_data_path)
 	for file in listfile:
-            #해당 디렉토리의 하위 디렉토리 폴더명을 찾음
-		if not(os.path.isdir(input_data_path+file)): #ignore .DS_Store
+        #Nur Verzeichnisse werden weiterverarbeitet
+		if not(os.path.isdir(input_data_path+file)): #ignore .DS_Store - Mac-spezfisches Problem...
 			continue
-		word=file+'/'
-		fullfilename=os.listdir(input_data_path+word)
-        # 하위디렉토리의 모든 비디오들의 이름을 저장
+		word=file+'/' # z.B. 'Computer/'
+		fullfilename=os.listdir(input_data_path+word) #Ganzer Pfad zu einem Wort-Ordner
+        # 하위디렉토리의 모든 비디오들의 이름을 저장 = Speichern Sie die Namen aller Videos im Unterverzeichnis
+		# Es werden die Ordner mit _ angelegt, falls noch nicht vorhanden. z.B. '_Computer'
 		if not(os.path.isdir(output_data_path+"_"+word)):
 			os.mkdir(output_data_path+"_"+word)
 		if not(os.path.isdir(output_data_path+word)):
 			os.mkdir(output_data_path+word)
-		os.system(comp)
+		os.system(comp) #MediaPipe Build ausführen
+		
 		outputfilelist=os.listdir(output_data_path+'_'+word)
+		# Für jede 
 		for mp4list in fullfilename:
 			if ".DS_Store" in mp4list:
 				continue         
