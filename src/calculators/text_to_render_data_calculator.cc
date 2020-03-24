de8183c45d0f05ef3c929c667e94ddad3c3f9b8a
Marius <Chuddle@web.de>
2020-03-24 17:52:07 +0100

    Added Text Overlay for prediction graph

--- src/calculators/text_to_render_data_calculator.cc
+++ src/calculators/text_to_render_data_calculator.cc
@@ -1,10 +1,11 @@
 
 #include "mediapipe/framework/calculator_framework.h"
 #include "mediapipe/framework/port/status.h"
-#include "mediapipe/framework/port/status_builder.h"
 #include "mediapipe/util/render_data.pb.h"
 
-namespace mediapipe {
+using namespace mediapipe;
+
+namespace signlang {
 
 // Takes in a std::string, draws the text std::string by cv::putText(), and
 // outputs an ImageFrame.
@@ -13,10 +14,8 @@ namespace mediapipe {
 // node {
 //   calculator: "TextToRenderDataCalculator"
 //   input_stream: "text_to_put"
-//   input_stream: "image_frame"
-//   output_stream: "out_image_frames"
+//   output_stream: "render_data"
 // }
-// TODO: Generalize the calculator for other text use cases.
 class TextToRenderDataCalculator : public CalculatorBase {
  public:
   static ::mediapipe::Status GetContract(CalculatorContract* cc);
@@ -24,20 +23,32 @@ class TextToRenderDataCalculator : public CalculatorBase {
 };
 
 ::mediapipe::Status TextToRenderDataCalculator::GetContract(CalculatorContract* cc) {
-  cc->Inputs().Index(0).Set<std::string>();
+  cc->Inputs().Index(0).SetAny();
   cc->Outputs().Index(0).Set<mediapipe::RenderData>();
   return ::mediapipe::OkStatus();
 }
 
 ::mediapipe::Status TextToRenderDataCalculator::Process(CalculatorContext* cc) {
-  const std::string& text_content = cc->Inputs().Index(0).Get<std::string>();
-  cv::Mat mat = cv::Mat::zeros(640, 640, CV_8UC4);
-  cv::putText(mat, text_content, cv::Point(15, 70), cv::FONT_HERSHEY_PLAIN, 3,
-              cv::Scalar(255, 255, 0, 255), 4);
-  std::unique_ptr<ImageFrame> output_frame = absl::make_unique<ImageFrame>(
-      ImageFormat::SRGBA, mat.size().width, mat.size().height);
-  mat.copyTo(formats::MatView(output_frame.get()));
-  cc->Outputs().Index(0).Add(output_frame.release(), cc->InputTimestamp());
+  // As an example, please see also mediapipe/calculators/util/labels_to_render_data_calculator.cc
+  //const std::string& text_content = cc->Inputs().Index(0).Get<std::string>();
+  // For Debug purposes:
+  const std::string& text_content = "Test";
+  RenderData render_data;
+  auto* text_annotation = render_data.add_render_annotations();
+  text_annotation->set_thickness(2);
+  text_annotation->mutable_color()->set_r(255);
+  text_annotation->mutable_color()->set_g(0);
+  text_annotation->mutable_color()->set_b(0);
+ 
+  auto* text = text_annotation->mutable_text();
+  text->set_display_text(text_content);
+  text->set_font_height(30.0);
+  text->set_left(300.0);
+  text->set_baseline(300.0);
+  text->set_font_face(2);
+  cc->Outputs()
+      .Index(0)
+      .AddPacket(mediapipe::MakePacket<RenderData>(render_data).At(cc->InputTimestamp()));
   return ::mediapipe::OkStatus();
 }
 
--- src/commands.md
+++ src/commands.md
@@ -1,21 +1,33 @@
 # Command list
+
 ### Information
+
 - Follow the installation guide of MediaPipe first.
 - All commands are relative to this folder. (`src/`)
 
 
-## Files to CSV on CPU
+## Files to CSV on CPU (Currently not working!)
+
 ### Build the source files
+
 `bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 app:files_to_csv_cpu`
+
 ### Run the application
+
 `GLOG_logtostderr=1 bazel-bin/app/files_to_csv_cpu --calculator_graph_config_file=graphs/video_processing_cpu.pbtxt`
 
 ## Files to CSV on GPU
+
 ### Build the source files
+
 `bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADERS app:files_to_csv_gpu`
+
 ### Run the application
+
 `GLOG_logtostderr=1 bazel-bin/app/files_to_csv_gpu --calculator_graph_config_file=graphs/video_processing_gpu.pbtxt`
+
 #### Options
+
 Processing Videos in a given directory requires two arguments:
 1. The input root directory containing subdirectories with .mp4 files. `--input_video_path=/home/datagroup/Videos/SL/input`
    Example for a structure: 
@@ -29,8 +41,14 @@ Processing Videos in a given directory requires two arguments:
     -- word_three
         -- file_four.mp4
    ```
-<<<<<<< HEAD
 2. The ouput root directory. Can be empty. `--output_video_path=/home/datagroup/Videos/SL/output`
-=======
-2. The ouput root directory. Can be empty. `--output_video_path="/home/Videos/SL/output`
->>>>>>> master
+
+## Sign Language Prediction
+
+### Build the source files
+
+`bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADERS app:prediction_gpu`
+
+### Run the application
+
+`GLOG_logtostderr=1 bazel-bin/app/prediction_gpu --calculator_graph_config_file=graphs/sign_lang_prediction_gpu.pbtxt`
