60107545b5da94682e7bdafdbca47dcd746e8bc9
Tachionstrahl <marius_martin@t-online.de>
2020-07-31 15:16:46 +0200

    Parallel conversion

--- convert_files.py
+++ convert_files.py
@@ -3,18 +3,33 @@
 
 import os
 import sys
+import multiprocessing as mp
 def start():
+    pool = mp.Pool(mp.cpu_count())
     os.system("cd src/ &&  bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADERS app:files_to_csv_gpu")
-    input_dir = "/home/michi/Videos/4_zu_3/"
+    input_dir = "/home/signlang/Videos/input/"
+
+    filenames = []
     for subdir, dirs, files in os.walk(input_dir):
         for file in files:
-            filename = os.path.join(subdir, file)
-            result = os.system("cd src/ && GLOG_logtostderr=0 bazel-bin/app/files_to_csv_gpu \
-            --calculator_graph_config_file=graphs/video_processing_gpu.pbtxt \
-            --input_video_path="+ filename + " \
-            --output_video_path=/home/michi/Videos/4_zu_3_rendered_2D/")
-            print(subdir, file, result)
+            filenames.append(os.path.join(subdir, file))
+    pool.map_async(doConversion, [file for file in filenames]).get()
+    # for subdir, dirs, files in os.walk(input_dir):
+    #     for file in files:
+    #         filename = os.path.join(subdir, file)
+    #         result = os.system("cd src/ && GLOG_logtostderr=0 bazel-bin/app/files_to_csv_gpu \
+    #         --calculator_graph_config_file=graphs/video_processing_gpu.pbtxt \
+    #         --input_video_path="+ filename + " \
+    #         --output_video_path=/home/signlang/Videos/output_relative_2D/")
+    #         print(subdir, file, result)
             
+
+def doConversion(filename):
+    result = os.system("cd src/ && GLOG_logtostderr=0 bazel-bin/app/files_to_csv_gpu \
+    --calculator_graph_config_file=graphs/video_processing_gpu.pbtxt \
+    --input_video_path="+ filename + " \
+    --output_video_path=/home/signlang/Videos/output_relative_2D/")
+    print (filename, result)
 
 if __name__ == "__main__":
     start()
